11c6a8ad6f3d085a52ddf9fc46f0f4a4b06cacc4
Yashwanth Muddana <muddana.yashwanth@gmail.com>
2022-02-21 21:49:44 +0530

    new changes

--- src/components/Home/Waitlist.js
+++ src/components/Home/Waitlist.js
@@ -4,18 +4,22 @@ import React, { useState } from "react";
 import imgEmail from "../../Assets/images/email.png";
 import "../../scss/waitlist.scss";
 import { toast } from "react-toastify";
+import { useEffect } from "react";
 function Waitlist() {
   const [email, setEmail] = useState("");
+  const [count, setCount] = useState(0);
   // const handleClick = () => {
   //   window.open("https://forms.gle/5kPVkp29WQji4iUV9");
   // };
   const userCollection = collection(firedb, "users");
+  useEffect(() => {
+    return getDocs(userCollection).then((res) => console.log(res.docs.length));
+  }, []);
   const addData = async (e) => {
     e.preventDefault();
     if (email.length >= 1) {
       await addDoc(userCollection, { email }).then((res) => {
         setEmail("");
-        console.log(res);
         toast.success("Yay! You are onSync ", {
           position: "top-center",
           autoClose: 3000,
@@ -30,12 +34,15 @@ function Waitlist() {
       });
     }
   };
+
   return (
     <div className="waitlistDiv" id="waitlist">
       <div className="waitlist">
         <img src={imgEmail} alt="onSync" className="waitlistImg" />
         <div className="waitlistContext">
-          <p className="waitlistText">Join 569 more people in the waitlist</p>
+          <p className="waitlistText">
+            Join {count ? 500 + count : 27} more people in the waitlist
+          </p>
           <form className="waitlistForm" onSubmit={addData}>
             <input
               type="email"
--- src/components/common/Navbar.js
+++ src/components/common/Navbar.js
@@ -1,11 +1,18 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import "../../scss/common/navbar.scss";
 import logo from "../../Assets/logos/logo.svg";
 import logoonyellow from "../../Assets/logos/logoonyellow.svg";
 import MenuIcon from "@mui/icons-material/Menu";
 import { links } from "../Data/navLinks";
 import { HashLink } from "react-router-hash-link";
+import { Button } from "@mui/material";
+import CloseIcon from "@mui/icons-material/Close";
+import { addDoc, collection, getDocs } from "firebase/firestore";
+import { firedb } from "../../firebase/firebase.config";
+import { toast } from "react-toastify";
+
 function Navbar() {
+  const [email, setEmail] = useState("");
   useEffect(() => {
     window.addEventListener("scroll", () => {
       console.log(window.pageYOffset);
@@ -16,11 +23,43 @@ function Navbar() {
       }
     });
   }, []);
+
+  // const handleClick = () => {
+  //   window.open("https://forms.gle/5kPVkp29WQji4iUV9");
+  // };
+  const userCollection = collection(firedb, "users");
+  const addData = async (e) => {
+    e.preventDefault();
+    if (email.length >= 1) {
+      await addDoc(userCollection, { email }).then((res) => {
+        setEmail("");
+        console.log(res);
+        toast.success("Yay! You are onSync ", {
+          position: "top-center",
+          autoClose: 3000,
+          theme: "colored",
+        });
+      });
+    } else {
+      toast.warning("Please enter a valid email", {
+        position: "top-center",
+        autoClose: 3000,
+        theme: "colored",
+      });
+    }
+  };
   return (
     <div id="nav" className="navbarDiv ">
       <div className="navbar">
         <img src={logo} alt="onSync" className="logo" />
-        <MenuIcon className="menuBtn" />
+        <Button
+          style={{ color: "#333", borderRadius: "7px" }}
+          onClick={() =>
+            document.getElementById("sm-nav").classList.toggle("active")
+          }
+        >
+          <MenuIcon className="menuBtn" />
+        </Button>
         <div className="navbar-menu">
           {links.map((link) => {
             return (
@@ -36,28 +75,61 @@ function Navbar() {
             );
           })}
         </div>
-        <div className="navbar-menu-sm">
+        <div className="navbar-menu-sm" id="sm-nav">
+          <Button
+            style={{
+              position: "absolute",
+              right: "0px",
+              top: "10px",
+              // margin: "5px",
+            }}
+            onClick={() =>
+              document.getElementById("sm-nav").classList.toggle("active")
+            }
+          >
+            <CloseIcon style={{ color: "white", fontSize: "30px" }} />
+          </Button>
           <img
             src={logoonyellow}
             alt="onSync"
             className="navbar-menu-sm-logo"
           />
           <div className="navbar-menu-items-sm">
-            <li className="navbar-item-sm">Home</li>
+            {/* <li className="navbar-item-sm">Home</li>
             <li className="navbar-item-sm">Features</li>
             <li className="navbar-item-sm">Universities</li>
             <li className="navbar-item-sm">Our Team</li>
             <li className="navbar-item-sm">Join the waitlist</li>
-            <li className="navbar-item-sm">Join Our Team</li>
+            <li className="navbar-item-sm">Join Our Team</li> */}
+            {links.map((link) => {
+              return (
+                <HashLink
+                  smooth
+                  to={link.url}
+                  key={link.id}
+                  onClick={() =>
+                    document.getElementById("sm-nav").classList.toggle("active")
+                  }
+                  style={{ textDecoration: "none" }}
+                >
+                  <li className="navbar-item-sm"> {link.text}</li>
+                </HashLink>
+
+                // <div></div>
+              );
+            })}
           </div>
-          <div className="navbar-menu-sm-form">
+          <form className="navbar-menu-sm-form">
             <input
               className="navbar-input"
               type="text"
               placeholder="Your email address"
+              onChange={(e) => setEmail(e.target.value)}
             />
-            <button className="navbar-form-btn">Join the waitlist</button>
-          </div>
+            <button className="navbar-form-btn" onClick={addData}>
+              Join the waitlist
+            </button>
+          </form>
           <div className="navbar-menu-sm-contact"></div>
         </div>
       </div>
